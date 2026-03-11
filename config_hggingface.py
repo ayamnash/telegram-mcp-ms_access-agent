c738127c5323dfeb30d94fa39ac9adc7a4dd6033
ayamnash <81065555+ayamnash@users.noreply.github.com>
2026-03-11 08:32:58 +0300

    Clear BOT_TOKEN and HUGGINGFACE_API_KEY values
    Removed sensitive information from configuration file.

--- config_hggingface.py
+++ config_hggingface.py
@@ -1,5 +1,5 @@
-BOT_TOKEN = "8652787347:AAEDC_wojoGUNluJiq7yiXpNwwnGQCv2dpg"
-HUGGINGFACE_API_KEY = "from huggingface"
+BOT_TOKEN = ""
+HUGGINGFACE_API_KEY = ""
 HUGGINGFACE_BASE_URL = "https://router.huggingface.co/v1"
 HUGGINGFACE_MODEL = "openai/gpt-oss-20b:groq"
 # python location   
@@ -9,3 +9,4 @@ MCP_COMMAND = r"C:\Users\it\AppData\Local\Programs\Python\Python311-32\python.ex
 MCP_SCRIPT = r"k:\bot_call_mcpmsaccess\server.py"
 
 DEFAULT_DB = r"k:\bot_call_mcpmsaccess\invoice.accdb"
+
